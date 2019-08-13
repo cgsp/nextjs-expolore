9876812e48b39f1ed775a40da59e4667b9fc1c4f
guansaipeng <cgsp740178088@163.com>
2019-08-13 23:16:54 +0800

    改变下脚手架的命令

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@
  * @Author: John.Guan
  * @Date: 2019-07-24 22:23:35
  * @Last Modified by: John.Guan
- * @Last Modified time: 2019-08-13 23:12:29
+ * @Last Modified time: 2019-08-13 23:14:20
  */
 
 //  在写next的时候，不需要，在文件的顶部，import react,因为，next已经全局处理了这个
 export default () => {
-  return <div>这是个页面hhahha 1</div>
+  return <div>这是个页面hhahh11222a 1</div>
 }
