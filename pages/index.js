34064fc9faf5c09e47f52274c235db5ebca737ab
guansaipeng <cgsp740178088@163.com>
2019-08-13 23:13:04 +0800

    改变配置命令,忽略编译

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,10 @@
  * @Author: John.Guan
  * @Date: 2019-07-24 22:23:35
  * @Last Modified by: John.Guan
- * @Last Modified time: 2019-08-13 23:06:23
+ * @Last Modified time: 2019-08-13 23:12:29
  */
 
-import './style.css'
 //  在写next的时候，不需要，在文件的顶部，import react,因为，next已经全局处理了这个
 export default () => {
-  return <div>这是个页面1111</div>
+  return <div>这是个页面hhahha 1</div>
 }
