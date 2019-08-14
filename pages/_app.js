26781f11d69b0af4c7165383e375608b468e4f6d
guansaipeng <cgsp740178088@163.com>
2019-08-14 10:54:59 +0800

    集成antd到next

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+/*
+ * @Desc: 覆盖next.js默认的的app组件
+ * @Author: John.Guan
+ * @Date: 2019-08-14 10:40:42
+ * @Last Modified by: John.Guan
+ * @Last Modified time: 2019-08-14 10:45:21
+ */
+
+// import 'antd/dist/antd.css'
+
+import App from 'next/app'
+
+export default App
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,16 @@
  * @Author: John.Guan
  * @Date: 2019-07-24 22:23:35
  * @Last Modified by: John.Guan
- * @Last Modified time: 2019-08-14 10:19:38
+ * @Last Modified time: 2019-08-14 10:43:01
  */
 
-import './style.css'
+import { Button } from 'antd'
 
 //  在写next的时候，不需要，在文件的顶部，import react,因为，next已经全局处理了这个
 export default () => {
-  return <div>这是个页</div>
+  return (
+    <div>
+      <Button type="primary">按钮</Button>
+    </div>
+  )
 }
